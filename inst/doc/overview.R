18d74503c6a12ea6756911333f66dfbe54a24eaa
Winston Chang <winston@posit.co>
2025-04-10 09:00:02 +0000

    version 1.4.4

--- inst/doc/overview.R
+++ inst/doc/overview.R
@@ -5,37 +5,37 @@ knitr::opts_chunk$set(
 )
 
 ## ----eval = FALSE-------------------------------------------------------------
-#  ws <- WebSocket$new("ws://echo.websocket.org/")
+# ws <- WebSocket$new("ws://echo.websocket.org/")
 
 ## ----eval = FALSE-------------------------------------------------------------
-#  ws <- WebSocket$new("ws://echo.websocket.org/", autoConnect = FALSE)
-#  # Set up callbacks here...
-#  ws$connect()
+# ws <- WebSocket$new("ws://echo.websocket.org/", autoConnect = FALSE)
+# # Set up callbacks here...
+# ws$connect()
 
 ## ----eval = FALSE-------------------------------------------------------------
-#  {
-#    ws <- WebSocket$new("ws://echo.websocket.org/")
-#    ws$onOpen(function(event) { message("websocket opened") })
-#  }
+# {
+#   ws <- WebSocket$new("ws://echo.websocket.org/")
+#   ws$onOpen(function(event) { message("websocket opened") })
+# }
 
 ## ----eval = FALSE-------------------------------------------------------------
-#  {
-#    ws <- WebSocket$new("ws://echo.websocket.org/")
-#    ws$onOpen(function(event) {
-#      cat("connected\n")
-#    })
-#  }
+# {
+#   ws <- WebSocket$new("ws://echo.websocket.org/")
+#   ws$onOpen(function(event) {
+#     cat("connected\n")
+#   })
+# }
 
 ## ----eval = FALSE-------------------------------------------------------------
-#  {
-#    ws <- WebSocket$new("ws://echo.websocket.org/")
-#    removeThis <- ws$onMessage(function(event) {
-#      cat("this is the last time i'll run\n")
-#      removeThis()
-#    })
-#    ws$onOpen(function(event) {
-#      ws$send("one")
-#      ws$send("two")
-#    })
-#  }
+# {
+#   ws <- WebSocket$new("ws://echo.websocket.org/")
+#   removeThis <- ws$onMessage(function(event) {
+#     cat("this is the last time i'll run\n")
+#     removeThis()
+#   })
+#   ws$onOpen(function(event) {
+#     ws$send("one")
+#     ws$send("two")
+#   })
+# }
 
